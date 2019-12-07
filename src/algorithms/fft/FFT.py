43a5d1e998ab652f4a3a30e93f9cbd54a6a833a1
BenxinNiu <benxin.niu@mun.ca>
2019-12-06 22:08:59 -0500

    fix fft

--- src/algorithms/fft/FFT.py
+++ src/algorithms/fft/FFT.py
@@ -21,4 +21,4 @@ def run(data):
     # TODO:::: this computes a double sided spectrum. We may wanna compute only for single sided
     fft = np.fft.fft(data)
     # encapsulate result into ResultEncapsulation object for easier integration
-    return ResultEncapsulation(result=fft, resultType=SupportedAlgorithms.FFT)
+    return ResultEncapsulation(result=fft, inputData=data, resultType=SupportedAlgorithms.FFT)
