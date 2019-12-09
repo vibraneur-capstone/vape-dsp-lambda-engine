4eecc58963b56310ac41931ed9e2ba0f6dea62ba
BenxinNiu <benxin.niu@mun.ca>
2019-12-08 20:33:01 -0500

    add scripts

--- scripts/aws_sam.sh
+++ scripts/aws_sam.sh
@@ -9,10 +9,10 @@ if [ "$len" == 0 ]; then
   exit 1
 elif [ "$1" == "package" ]; then
   echo packaging started
-  sam package --template-file dsp-engine-sam.yaml --s3-bucket vape-artifacts --output-template-file packaged-dsp-sam.yaml
+  sam package --template-file ./../dsp-engine-sam.yaml --s3-bucket vape-artifacts --output-template-file ./../packaged-dsp-sam.yaml
 elif [ "$1" == "deploy" ]; then
   # --no-fail-on-empty-changeset
-  sam deploy --template-file ./packaged-dsp-sam.yaml --stack-name dsp-sam-test --region us-east-1 --capabilities CAPABILITY_IAM
+  sam deploy --template-file ./../packaged-dsp-sam.yaml --stack-name dsp-sam-test --region us-east-1 --capabilities CAPABILITY_IAM
 else
   echo No valid arguments provided... Existing...
   exit 1
